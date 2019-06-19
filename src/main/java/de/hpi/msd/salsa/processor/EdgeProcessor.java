4636da3588fadc20aae6d7087727b70400bb3694
Philipp Hager <philipp.konstantin.hager@gmail.com>
2019-06-19 15:39:49 +0200

    Adapt EdgeProcessor

--- src/main/java/de/hpi/msd/salsa/processor/EdgeProcessor.java
+++ src/main/java/de/hpi/msd/salsa/processor/EdgeProcessor.java
@@ -3,30 +3,29 @@ package de.hpi.msd.salsa.processor;
 
 import de.hpi.msd.salsa.EdgeToAdjacencyApp;
 import de.hpi.msd.salsa.serde.avro.Edge;
-import de.hpi.msd.salsa.store.TweetAdjacencyStore;
-import de.hpi.msd.salsa.store.TweetStateStoreBuilder;
-import de.hpi.msd.salsa.store.UserAdjacencyStore;
+import de.hpi.msd.salsa.store.SegmentedStateStore;
+import de.hpi.msd.salsa.store.SegmentedStateStoreBuilder;
 import org.apache.kafka.streams.processor.AbstractProcessor;
 import org.apache.kafka.streams.processor.ProcessorContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EdgeProcessor extends AbstractProcessor<byte[], Edge> {
-    private final Logger logger = LoggerFactory.getLogger(TweetStateStoreBuilder.class);
-    private UserAdjacencyStore userStore;
-    private TweetAdjacencyStore tweetStore;
+    private final Logger logger = LoggerFactory.getLogger(SegmentedStateStoreBuilder.class);
+    private SegmentedStateStore leftIndex;
+    private SegmentedStateStore rightIndex;
 
     @Override
     public void init(ProcessorContext processorContext) {
         super.init(processorContext);
-        userStore = (UserAdjacencyStore) processorContext.getStateStore(EdgeToAdjacencyApp.LEFT_INDEX_NAME);
-        tweetStore = (TweetAdjacencyStore) processorContext.getStateStore(EdgeToAdjacencyApp.RIGHT_INDEX_NAME);
+        leftIndex = (SegmentedStateStore) processorContext.getStateStore(EdgeToAdjacencyApp.LEFT_INDEX_NAME);
+        rightIndex = (SegmentedStateStore) processorContext.getStateStore(EdgeToAdjacencyApp.RIGHT_INDEX_NAME);
     }
 
     @Override
     public void process(byte[] bytes, Edge edge) {
         logger.debug("Processing: %s", edge);
-        userStore.write(edge);
-        tweetStore.write(edge);
+        leftIndex.write(edge.getUserId(), edge.getTweedId(), edge.getEdgeType());
+        rightIndex.write(edge.getTweedId(), edge.getUserId(), edge.getEdgeType());
     }
 }
