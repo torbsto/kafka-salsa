3d72cb9ce164e6c4a20d5c7035de032498cd7a55
Philipp Hager <philipp.konstantin.hager@gmail.com>
2019-06-20 14:56:04 +0200

    Add message to SegmentFullException

--- src/main/java/de/hpi/msd/salsa/store/index/SegmentFullException.java
+++ src/main/java/de/hpi/msd/salsa/store/index/SegmentFullException.java
@@ -3,4 +3,8 @@ package de.hpi.msd.salsa.store.index;
 public final class SegmentFullException extends IndexOutOfBoundsException {
     public SegmentFullException() {
     }
+
+    public SegmentFullException(String s) {
+        super(s);
+    }
 }
