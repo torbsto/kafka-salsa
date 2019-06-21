6174c6d9144d12ce09d1e7e6f946c59e127f93e2
Philipp Hager <philipp.konstantin.hager@gmail.com>
2019-06-21 09:56:55 +0200

    Revert EdgeProcessor to simple implementation

--- src/main/java/de/hpi/msd/salsa/processor/EdgeProcessor.java
+++ src/main/java/de/hpi/msd/salsa/processor/EdgeProcessor.java
@@ -2,30 +2,43 @@ package de.hpi.msd.salsa.processor;
 
 
 import de.hpi.msd.salsa.EdgeToAdjacencyApp;
+import de.hpi.msd.salsa.serde.avro.AdjacencyList;
 import de.hpi.msd.salsa.serde.avro.Edge;
-import de.hpi.msd.salsa.store.SegmentedStateStore;
-import de.hpi.msd.salsa.store.SegmentedStateStoreBuilder;
 import org.apache.kafka.streams.processor.AbstractProcessor;
 import org.apache.kafka.streams.processor.ProcessorContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+import java.util.Collections;
 
 public class EdgeProcessor extends AbstractProcessor<byte[], Edge> {
-    private final Logger logger = LoggerFactory.getLogger(SegmentedStateStoreBuilder.class);
-    private SegmentedStateStore leftIndex;
-    private SegmentedStateStore rightIndex;
+    private KeyValueStore<Long, AdjacencyList> leftIndex;
+    private KeyValueStore<Long, AdjacencyList> rightIndex;
 
     @Override
     public void init(ProcessorContext processorContext) {
         super.init(processorContext);
-        leftIndex = (SegmentedStateStore) processorContext.getStateStore(EdgeToAdjacencyApp.LEFT_INDEX_NAME);
-        rightIndex = (SegmentedStateStore) processorContext.getStateStore(EdgeToAdjacencyApp.RIGHT_INDEX_NAME);
+        leftIndex = (KeyValueStore<Long, AdjacencyList>) processorContext.getStateStore(EdgeToAdjacencyApp.LEFT_INDEX_NAME);
+        rightIndex = (KeyValueStore<Long, AdjacencyList>) processorContext.getStateStore(EdgeToAdjacencyApp.RIGHT_INDEX_NAME);
     }
 
     @Override
     public void process(byte[] bytes, Edge edge) {
-        logger.debug("Processing: %s", edge);
-        leftIndex.write(edge.getUserId(), edge.getTweedId(), edge.getEdgeType());
-        rightIndex.write(edge.getTweedId(), edge.getUserId(), edge.getEdgeType());
+        AdjacencyList tweets = getAdjacencyList(edge.getUserId(), edge.getTweedId(), leftIndex);
+        leftIndex.put(edge.getUserId(), tweets);
+
+        AdjacencyList user = getAdjacencyList(edge.getTweedId(), edge.getUserId(), rightIndex);
+        rightIndex.put(edge.getTweedId(), user);
+
+        context().forward(edge.getUserId(), tweets);
+    }
+
+    private AdjacencyList getAdjacencyList(Long leftId, Long rightId, KeyValueStore<Long, AdjacencyList> index) {
+        AdjacencyList currentNeighbors = index.get(leftId);
+        if (currentNeighbors == null) {
+            currentNeighbors = new AdjacencyList(Collections.singletonList(rightId));
+        } else {
+            currentNeighbors.getNeighbors().add(rightId);
+        }
+        return currentNeighbors;
     }
 }
