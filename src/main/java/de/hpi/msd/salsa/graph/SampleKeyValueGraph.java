07dfb4136772a3072da2941e206519cb63d83c0e
torbsto <torben.meyer2@student.hpi.de>
2019-07-01 22:45:01 +0200

    Update SamplingGraph

--- src/main/java/de/hpi/msd/salsa/graph/SampleKeyValueGraph.java
+++ src/main/java/de/hpi/msd/salsa/graph/SampleKeyValueGraph.java
@@ -1,28 +1,41 @@
 package de.hpi.msd.salsa.graph;
 
-import de.hpi.msd.salsa.serde.avro.SampledAdjacencyList;
+import de.hpi.msd.salsa.serde.avro.RangeKey;
+import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class SampledLocalKeyValueGraph extends KeyValueGraph {
-    private ReadOnlyKeyValueStore<Long, SampledAdjacencyList> leftIndex;
-    private ReadOnlyKeyValueStore<Long, SampledAdjacencyList> rightIndex;
+public class SampleKeyValueGraph extends KeyValueGraph {
+    private ReadOnlyKeyValueStore<RangeKey, Long> leftIndex;
+    private ReadOnlyKeyValueStore<RangeKey, Long> rightIndex;
+    private int bufferSize;
 
-    public SampledLocalKeyValueGraph(ReadOnlyKeyValueStore<Long, SampledAdjacencyList> leftIndex,
-                                     ReadOnlyKeyValueStore<Long, SampledAdjacencyList> rightIndex) {
+    public SampleKeyValueGraph(ReadOnlyKeyValueStore<RangeKey, Long> leftIndex,
+                               ReadOnlyKeyValueStore<RangeKey, Long> rightIndex,
+                               int bufferSize) {
         this.leftIndex = leftIndex;
         this.rightIndex = rightIndex;
+        this.bufferSize = bufferSize;
     }
 
     @Override
     public List<Long> getLeftNodeNeighbors(long nodeId) {
-        return leftIndex.get(nodeId).getNeighbors();
+        return getNeighbors(nodeId, leftIndex);
     }
 
 
     @Override
     public List<Long> getRightNodeNeighbors(long nodeId) {
-        return rightIndex.get(nodeId).getNeighbors();
+        return getNeighbors(nodeId, rightIndex);
     }
+
+    private List<Long> getNeighbors(long nodeId, ReadOnlyKeyValueStore<RangeKey, Long> index) {
+        KeyValueIterator<RangeKey, Long> iterator = index.range(new RangeKey(nodeId, 0L), new RangeKey(nodeId, (long) bufferSize));
+        List<Long> list = new ArrayList<>();
+        iterator.forEachRemaining(k -> list.add(k.value));
+        return list;
+    }
+
 }
