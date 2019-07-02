422f7afd8718caefe1227900d2a19af31310d274
torbsto <torben.meyer2@student.hpi.de>
2019-07-02 09:36:33 +0200

    Add sampling app

---
+++ src/main/java/de/hpi/msd/salsa/commands/SamplingApp.java
@@ -0,0 +1,66 @@
+package de.hpi.msd.salsa.commands;
+
+import de.hpi.msd.salsa.graph.BipartiteGraph;
+import de.hpi.msd.salsa.graph.rangeKey.SampleKeyValueGraph;
+import de.hpi.msd.salsa.processor.SamplingEdgeProcessor;
+import de.hpi.msd.salsa.serde.avro.RangeKey;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.Stores;
+import picocli.CommandLine;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+
+@CommandLine.Command(name = "sampling", mixinStandardHelpOptions = true,
+        description = "Sampling Edge Processor")
+public class SamplingApp extends BaseKafkaSalsaApp {
+    public final static String LEFT_INDEX_NAME = "leftIndex";
+    public final static String RIGHT_INDEX_NAME = "rightIndex";
+
+    @CommandLine.Option(names = "--buffer", defaultValue = "5000", description = "Buffer for reservoir sampling")
+    private int bufferSize = 5000;
+
+
+    @Override
+    BipartiteGraph getGraph(KafkaStreams streams) {
+        return new SampleKeyValueGraph(
+                streams.store(LEFT_INDEX_NAME, QueryableStoreTypes.keyValueStore()),
+                streams.store(RIGHT_INDEX_NAME, QueryableStoreTypes.keyValueStore()),
+                bufferSize);
+    }
+
+    @Override
+    Topology getTopology(Properties properties) {
+        final Map<String, String> serdeConfig = Collections.singletonMap(
+                AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+        final SpecificAvroSerde<RangeKey> rangeKeySerde = new SpecificAvroSerde<>();
+        rangeKeySerde.configure(serdeConfig, true);
+
+        return new Topology()
+                .addSource("Edge-Source", topicName)
+                .addProcessor("EdgeProcessor", () -> new SamplingEdgeProcessor(bufferSize), "Edge-Source")
+                .addStateStore(Stores.keyValueStoreBuilder(
+                        Stores.inMemoryKeyValueStore(LEFT_INDEX_NAME),
+                        rangeKeySerde, Serdes.Long()), "EdgeProcessor")
+                .addStateStore(Stores.keyValueStoreBuilder(
+                        Stores.inMemoryKeyValueStore(RIGHT_INDEX_NAME),
+                        rangeKeySerde, Serdes.Long()), "EdgeProcessor")
+                .addStateStore(Stores.keyValueStoreBuilder(
+                        Stores.inMemoryKeyValueStore("leftCount"),
+                        Serdes.Long(), Serdes.Long()), "EdgeProcessor")
+                .addStateStore(Stores.keyValueStoreBuilder(
+                        Stores.inMemoryKeyValueStore("rightCount"),
+                        Serdes.Long(), Serdes.Long()), "EdgeProcessor");
+    }
+
+    public static void main(String[] args) {
+        CommandLine commandLine = new CommandLine(new SamplingApp());
+        commandLine.execute(args);
+    }
+}
