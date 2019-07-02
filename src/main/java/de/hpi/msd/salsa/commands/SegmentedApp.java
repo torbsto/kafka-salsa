3b85f7346b5faa05e9a864870faf02654fa7b77c
torbsto <torben.meyer2@student.hpi.de>
2019-07-02 09:42:14 +0200

    Add segmented app

---
+++ src/main/java/de/hpi/msd/salsa/commands/SegmentedApp.java
@@ -0,0 +1,58 @@
+package de.hpi.msd.salsa.commands;
+
+import de.hpi.msd.salsa.graph.BipartiteGraph;
+import de.hpi.msd.salsa.graph.segmented.SegmentedGraph;
+import de.hpi.msd.salsa.processor.SegmentedEdgeProcessor;
+import de.hpi.msd.salsa.serde.avro.SampledAdjacencyList;
+import de.hpi.msd.salsa.store.EdgeReadableStateStoreType;
+import de.hpi.msd.salsa.store.SegmentedStateStoreBuilder;
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.Topology;
+import picocli.CommandLine;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+
+@CommandLine.Command(name = "simple", mixinStandardHelpOptions = true,
+        description = "Simple Edge Processor")
+public class SegmentedApp extends BaseKafkaSalsaApp {
+    @CommandLine.Option(names = "--segments", defaultValue = "10", description = "Segments inside graphjet index")
+    private int segments = 10;
+
+    @CommandLine.Option(names = "--pools", defaultValue = "16", description = "Pools inside graphjet segment")
+    private int poolsPerSegment = 16;
+
+    @CommandLine.Option(names = "--nodesPerPool", defaultValue = "131072", description = "Nodes per graphjet pool")
+    private int nodesPerPool = 131072;
+
+    @Override
+    BipartiteGraph getGraph(KafkaStreams streams) {
+        return new SegmentedGraph(
+                streams.store(LEFT_INDEX_NAME, new EdgeReadableStateStoreType()),
+                streams.store(RIGHT_INDEX_NAME, new EdgeReadableStateStoreType()));
+    }
+
+    @Override
+    Topology getTopology(Properties properties) {
+        final Map<String, String> serdeConfig = Collections.singletonMap(
+                AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+        final SpecificAvroSerde<SampledAdjacencyList> adjacencyListSerde = new SpecificAvroSerde<>();
+        adjacencyListSerde.configure(serdeConfig, true);
+
+        return new Topology()
+                .addSource("Edge-Source", topicName)
+                .addProcessor("EdgeProcessor", SegmentedEdgeProcessor::new, "Edge-Source")
+                .addStateStore(new SegmentedStateStoreBuilder(LEFT_INDEX_NAME, segments, poolsPerSegment, nodesPerPool),
+                        "EdgeProcessor")
+                .addStateStore(new SegmentedStateStoreBuilder(RIGHT_INDEX_NAME, segments, poolsPerSegment, nodesPerPool),
+                        "EdgeProcessor");
+    }
+
+    public static void main(String[] args) {
+        CommandLine commandLine = new CommandLine(new SegmentedApp());
+        commandLine.execute(args);
+    }
+}
